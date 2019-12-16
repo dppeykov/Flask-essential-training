9bf824478ff11e882cac6942ad91fb0f2e38ed5d
Damyan Peykov <dppeykov@gmail.com>
2019-12-16 09:18:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return render_template("template-home.html") # needs to be in the /templates folder in the project - not included in this repo - e.g. /templates/home.html
+  # the template html needs to be in the /templates folder in the project - not included in this repo - e.g. /templates/home.html
+  return render_template("template-home.html") # by adding , name="X-men" in the () we can pass the value to the html template 
+
+
 
 @app.route('/about')
 def about():
