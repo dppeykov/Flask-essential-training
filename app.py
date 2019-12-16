a3e4b79f72005f3b9623dc201bae505c7c420a9a
Damyan Peykov <dppeykov@gmail.com>
2019-12-16 09:15:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return render_template("template-home.html")
+  return render_template("template-home.html") # needs to be in the /templates folder in the project - not included in this repo - e.g. /templates/home.html
 
 @app.route('/about')
 def about():
