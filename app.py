ec51ecf17f6b1f7e8d3d9eb33fbb2e419cf9217c
Damyan Peykov <dppeykov@gmail.com>
2019-12-16 10:12:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,4 +10,6 @@ def home():
 
 @app.route('/your-url')
 def your_url():
-  return render_template("template-your_url.html")
+  return render_template("template-your_url.html", code=request.args["code"]) 
+# a get request in the template-home.html will call the your-url function and pass the code variable to the template-your_url.html
+
